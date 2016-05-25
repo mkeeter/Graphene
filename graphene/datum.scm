ca911049f35387e9ea52b0452ba28dd633299cfe
Matt Keeter <matt.j.keeter@gmail.com>
2016-05-24 21:44:00 -0400

    Make datum GOOPier

--- graphene/datum.scm
+++ graphene/datum.scm
@@ -20,30 +20,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-module (graphene datum)
-    #:export (make-datum))
+    #:export (make-datum datum-set-expr! datum-eval! datum-value datum-error))
+
+(use-modules (oop goops))
+
+(define-class <datum> ()
+    (expr #:init-form "")
+    (value #:init-form #nil)
+    (error #:init-form #nil))
+
+(define (make-datum) (make <datum>))
+
+(define-method (datum-set-expr! (d <datum>) (e <string>))
+    "datum-set-expr! d s
+    Sets the datum's expr to the given value
+    Returns true if the expr changed, false otherwise"
+    (if (equal? (slot-ref d 'expr) e) #f
+        (begin (slot-set! d 'expr e) #t)))
+
+(define-method (datum-value (d <datum>))
+    (slot-ref d 'value))
+
+(define-method (datum-error (d <datum>))
+    (slot-ref d error))
+
+(define-method (datum-eval! (d <datum>) (env <module>))
+    "datum-eval! d env
+    Evaluates a datum in the given environment
+    If successful, sets value slot to result and error to nil
+    Otherwise, sets values slot to nil and error to the error
+    Returns true if value has changed, otherwise false"
+    (let ((prev (slot-ref d 'value)))
+    (catch #t
+    ;; Evaluate function in the provided environment
+    (lambda ()
+        (let ((out (eval (read (open-input-string (slot-ref d 'expr))) env)))
+            (slot-set! d 'error #nil)
+            (slot-set! d 'value out)
+            (not (equal? out prev))))
+    ;; On error, record the error message
+    (lambda (key . args)
+        (slot-set! d 'error (cons key args))
+        (slot-set! d 'value #nil)))
+    (not (equal? prev (slot-ref d 'value)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (make-datum)
-    (let ((expr #nil)
-          (value #nil)
-          (error #nil))
-        (define (eval-expr env)
-            (set! value
-                (catch #t
-                (lambda ()
-                    (let ((out (eval expr env)))
-                    (set! error #nil) out))
-                (lambda (key . args)
-                    (set! error (cons key args))
-                    #nil))))
-
-        (define (dispatch-datum key . args)
-            (cond ((eq? key 'value) value)
-                  ((eq? key 'error) error)
-                  ((eq? key 'expr) expr)
-                  ((eq? key 'type) 'datum)
-                  ((eq? key 'set-expr!) (set! expr (car args)))
-                  ((eq? key 'eval-expr) (eval-expr (car args)))
-                  (else (error "Invalid key" key))))
-        dispatch-datum))
